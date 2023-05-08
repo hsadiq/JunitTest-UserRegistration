da9fa8b9f0faf78d7c4a1b6318195e9445f5f48b
hsadiq <ksadik320@gmail.com>
2023-05-09 00:34:49 +0530

    Solving The Error For Freash Commit

--- src/test/java/com/bl/UserTest.java
+++ src/test/java/com/bl/UserTest.java
@@ -8,7 +8,9 @@ public class UserTest {
 
     FirstName firstName = new FirstName();
     LastName lastName = new LastName();
-
+    Email email = new Email();
+    PhoneNumber phoneNumber = new PhoneNumber();
+    Password passwd = new Password();
 
     @BeforeAll
     static void beforeall(){
@@ -35,6 +37,22 @@ public class UserTest {
     @Test
     public void checking_Last(){
         boolean result = lastName.getLastName("Hussain");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void checkingEmail(){
+        boolean result = email.getEmail("abc.xyz@bl.co.in");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void checkingNumber(){
+        boolean result = phoneNumber.getMobile("91 7828606108");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void checkingPassword(){
+        boolean result = passwd.getPasswd("Sadiq123@");
         Assertions.assertTrue(result);
     }
 }
